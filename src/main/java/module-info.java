7d8031703d5005b625c63fe10db4c93e0b58cc7e
Sebastian Strobl <Sebastian@Strobl.net>
2019-07-07 01:14:16 +0200

    gradle support finished except for creation of jars

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module autofinancials.main {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.graphics;
+    requires org.json;
+    requires java.sql;
+    requires opencsv;
+    opens net.strobl.management;
+    opens net.strobl.processing;
+    opens net.strobl.frontend.general to javafx.fxml;
+    opens net.strobl.frontend.tabs.dashboard to javafx.fxml;
+    opens net.strobl.frontend.tabs.bills to javafx.fxml;
+    opens net.strobl.frontend.tabs.projects to javafx.fxml;
+    opens net.strobl.frontend.tabs.settings to javafx.fxml;
+}
---
+++ src/main/java/net/strobl/management/Launcher.java
@@ -0,0 +1,8 @@
+package net.strobl.management;
+
+public class Launcher {
+
+    public static void main(String[] args) {
+        App.main(args);
+    }
+}
